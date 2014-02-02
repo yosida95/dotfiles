38b5d97f46daf86012f6392baa48b33bfd695a0b
yosida95 <kohei@yosida95.com>
2014-02-03 03:39:46 +0900

    follow the fashion of oh-my-zsh

---
+++ oh-my-zsh_custom/plugins/git/git.plugin.zsh
@@ -0,0 +1,35 @@
+# Aliases
+
+#
+# Will return the current branch name
+# Usage example: git pull origin $(current_branch)
+#
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
+
+# Pretty log messages
+function _git_log_prettily(){
+  if ! [ -z $1 ]; then
+    git log --pretty=$1
+  fi
+}
+
+# Work In Progress (wip)
+# These features allow to pause a branch development and switch to another one (wip)
+# When you want to go back to work, just unwip it
+#
+# This function return a warning if the current branch is a wip
+function work_in_progress() {
+  if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
+    echo "WIP!!"
+  fi
+}
--- oh-my-zsh_custom/plugins/go-prompt/go-prompt.plugin.zsh
+++
@@ -1,8 +0,0 @@
-function go_prompt_info {
-    if which go > /dev/null 2>&1; then
-        _DISPLAY=$(basename $(dirname $(dirname $(which go))))
-        echo "$ZSH_THEME_GO_PROMPT_PREFIX$_DISPLAY$ZSH_THEME_GO_PROMPT_SUFFIX"
-        unset _DISPLAY
-    fi
-}
-
