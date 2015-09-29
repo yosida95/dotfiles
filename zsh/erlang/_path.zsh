d3a5e21833ccca69cd2f72288e65f0c725c279a1
yosida95 <kohei@yosida95.com>
2015-09-29 19:31:39 +0900

    reverse PATH to use new versions

--- zsh/erlang/_path.zsh
+++ zsh/erlang/_path.zsh
@@ -1,7 +1,7 @@
 # vim: set filetype=zsh :
 
 if [ -d /opt/erlang ]; then
-    find /opt/erlang -maxdepth 1 -mindepth 1 -print0| sort -r -z| while read -r -d $'\0' prefix; do
+    find /opt/erlang -maxdepth 1 -mindepth 1 -print0| sort -r| while read -r -d $'\0' prefix; do
         PATH=$prefix/bin:$PATH
     done
     unset prefix
