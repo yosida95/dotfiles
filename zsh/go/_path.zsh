394fa1d9ec411f47d69d325e7ad0740676e14436
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2015-10-09 14:03:13 +0900

    fix PATH versioning order

--- zsh/go/_path.zsh
+++ zsh/go/_path.zsh
@@ -1,7 +1,7 @@
 # vim: set filetype=zsh :
 
 if [ -d /opt/go ]; then
-    find /opt/go -maxdepth 1 -mindepth 1 -print0| sort -r| while read -r -d $'\0' prefix; do
+    find /opt/go -maxdepth 1 -mindepth 1 -print0| sort -nz| while read -r -d $'\0' prefix; do
         PATH=$prefix/bin:$PATH
         if [ -z "$GOROOT" ]; then
             export GOROOT=$prefix
--- zsh/node/_path.zsh
+++ zsh/node/_path.zsh
@@ -1,7 +1,7 @@
 # vim: set filetype=zsh :
 
 if [ -d /opt/node ]; then
-    find /opt/node -maxdepth 1 -mindepth 1 -print0| sort -r| while read -r -d $'\0' prefix; do
+    find /opt/node -maxdepth 1 -mindepth 1 -print0| sort -nz| while read -r -d $'\0' prefix; do
         PATH=$prefix/bin:$PATH
     done
     unset prefix
