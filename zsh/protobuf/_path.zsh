676a9deaec3631f7707ec31bfe7e6d7ad25e54d0
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2016-09-30 04:55:49 +0900

    add protoc in PATH

---
+++ zsh/protobuf/_path.zsh
@@ -0,0 +1,8 @@
+# vim: set filetype=zsh :
+
+if [ -d /opt/erlang ]; then
+    find /opt/protobuf -maxdepth 1 -mindepth 1 -print0| sort -nz| while read -r -d $'\0' prefix; do
+        PATH=$prefix/bin:$PATH
+    done
+    unset prefix
+fi
