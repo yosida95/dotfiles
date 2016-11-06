dc3a12400a7942fa66144c76f46f732a3602c9c3
yosida95 <kohei@yosida95.com>
2016-11-06 20:50:15 +0900

    enable mouse controling on tmux >= 2.1

--- tmux/source.sh
+++ tmux/source.sh
@@ -4,7 +4,9 @@ source_tmuxconf () {
     confdir="${0%/*}"
     tmux_version=`tmux -V| cut -d' ' -f2`
 
-    if [[ `echo "${tmux_version} >= 1.9"| bc` -eq 1 ]]; then
+    if [[ `echo "${tmux_version} >= 2.1"| bc` -eq 1 ]]; then
+        tmux source-file "$confdir/tmux_21_later.conf"
+    elif [[ `echo "${tmux_version} >= 1.9"| bc` -eq 1 ]]; then
         tmux source-file "$confdir/tmux_19_later.conf"
     else
         tmux source-file "$confdir/tmux_18_earlier.conf"
