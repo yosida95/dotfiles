6570914e80129b81f9e1075df2688aad6ae21188
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2018-01-15 15:04:14 +0900

    zsh: support gcloud instlattion with APT

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -6,8 +6,11 @@ fi
 
 if (($+commands[gcloud])); then
     file="${"$(which gcloud)":h:h}/completion.zsh.inc"
-    if [ -f "$file" ]; then
-        source "$file"
+    if [[ -f "$file" && -r "$file" ]]; then
+        . "$file"
+    elif [[ -f "/usr/share/google-cloud-sdk/completion.zsh.inc" &&
+            -r "/usr/share/google-cloud-sdk/completion.zsh.inc" ]]; then
+        . /usr/share/google-cloud-sdk/completion.zsh.inc
     fi
     unset file
 fi
