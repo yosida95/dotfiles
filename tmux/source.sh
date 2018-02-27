a99132b53f2119f584a265f82c484dfb4cbbd917
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2018-02-27 18:49:05 +0900

    tmux: remove config for tmux<=1.9,<2.1

--- tmux/source.sh
+++ tmux/source.sh
@@ -6,8 +6,6 @@ source_tmuxconf () {
 
     if [[ `echo "${tmux_version} >= 2.1"| bc` -eq 1 ]]; then
         tmux source-file "$confdir/tmux_21_later.conf"
-    elif [[ `echo "${tmux_version} >= 1.9"| bc` -eq 1 ]]; then
-        tmux source-file "$confdir/tmux_19_later.conf"
     else
         tmux source-file "$confdir/tmux_18_earlier.conf"
     fi
