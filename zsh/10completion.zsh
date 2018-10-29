eb6c1990fca6bb56875336519f46fa1c7c5268a9
Kohei YOSHIDA <kohei@yosida95.com>
2018-10-29 19:48:09 +0900

    zsh: clean indentation

--- zsh/10completion.zsh
+++ zsh/10completion.zsh
@@ -9,23 +9,23 @@ if (($+commands[kubectl])); then
 fi
 
 if (($+commands[gcloud])); then
+  for file in "${"$(which gcloud)":h:h}/completion.zsh.inc" "/usr/share/google-cloud-sdk/completion.zsh.inc"; do
     file="${"$(which gcloud)":h:h}/completion.zsh.inc"
     if [[ -f "$file" && -r "$file" ]]; then
         . "$file"
-    elif [[ -f "/usr/share/google-cloud-sdk/completion.zsh.inc" &&
-            -r "/usr/share/google-cloud-sdk/completion.zsh.inc" ]]; then
-        . /usr/share/google-cloud-sdk/completion.zsh.inc
+        break
     fi
-    unset file
+  done
+  unset file
 fi
 
 autoload -U bashcompinit && bashcompinit
 BASH_COMPLETION_DIR="/etc/bash_completion.d"
 for name in "hal"; do
-    name="${BASH_COMPLETION_DIR}/${name}"
-    if [[ -f $name && -r $name ]]; then
-        . $name
-    fi
+  name="${BASH_COMPLETION_DIR}/${name}"
+  if [[ -f $name && -r $name ]]; then
+    . $name
+  fi
 done
 unset name
 unset BASH_COMPLETION_DIR
