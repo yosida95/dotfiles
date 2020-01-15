85560f1e667f3550a96214ef010923f4984526ab
Kohei YOSHIDA <kohei@yosida95.com>
2020-01-15 16:30:04 +0900

    zsh: find the latest tmux and vim from /opt

--- zsh/01path.zsh
+++ zsh/01path.zsh
@@ -14,9 +14,26 @@ case "$(uname)" in
         ;;
 esac
 
-for prefix in /usr/lib/jvm /opt/erlang /opt/go /opt/gradle /opt/node /opt/protobuf /opt/python /opt/sbt /opt/scala /opt/vim; do
+versions() { find -L $1 -maxdepth 2 -name bin -type d -print0| sort -Vrz }
+
+# Emit only the latest version
+for prefix in /opt/tmux /opt/vim; do
     if [ -d "$prefix" ]; then
-        PATH="$(find -L $prefix -maxdepth 2 -name bin -type d -print0| sort -Vrz| tr '\0' ':')${PATH}"
+        PATH="$(versions $prefix| cut -d '' -f 1):${PATH}"
+    fi
+done
+
+# Emit all installed versions
+for prefix in /usr/lib/jvm /opt/gradle \
+              /opt/erlang \
+              /opt/go \
+              /opt/node \
+              /opt/protobuf \
+              /opt/python \
+              /opt/scala /opt/sbt; do
+    if [ -d "$prefix" ]; then
+        PATH="$(versions $prefix| tr '\0' ':')${PATH}"
+
         case "$prefix" in
             "/usr/lib/jvm")
                 export JAVA_HOME="${PATH%%/bin:*}"
@@ -27,24 +44,26 @@ for prefix in /usr/lib/jvm /opt/erlang /opt/go /opt/gradle /opt/node /opt/protob
         esac
     fi
 done
-unset prefix
 
-if [ -d "$HOME/.rbenv" ]; then
-    PATH=$HOME/.rbenv/bin:$PATH
-    eval "$(rbenv init -)"
-fi
+# Emit "$prefix/bin"
+for prefix in $HOME/.cargo $HOME/.local $HOME/.rbenv; do
+    if [ -d "$prefix/bin" ]; then
+        PATH=$prefix/bin:$PATH
 
-if [ -d "$HOME/.cargo/bin" ]; then
-    PATH=$HOME/.cargo/bin:$PATH
-fi
+        case "$prefix" in
+            "$HOME/.rbenv")
+                eval "$(rbenv init -)"
+                ;;
+        esac
+    fi
+done
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "${HOME}/.local/google-cloud-sdk/path.zsh.inc" ]; then
     source "${HOME}/.local/google-cloud-sdk/path.zsh.inc";
 fi
 
-if [ -d "$HOME/.local/bin" ]; then
-    PATH=$HOME/.local/bin:$PATH
-fi
 
+unset prefix
+unfunction versions
 export PATH
