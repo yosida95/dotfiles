yosida95/dotfiles

commit 0a135a04c6a50149bfc4c9622a8c7147d9d9351b

zsh: dedup $PATH and $FPATH, control variable scope by anonymous functions

zsh/01path.zsh
@@ -4,54 +4,64 @@ else
   PATH=$_ZSHRC_ORIG_PATH
 fi
 
+# deduplicate $PATH (keep the first occurrence)
+typeset -U PATH path
+export PATH
+
 case "$(uname)" in
   "Darwin")
-    for prefix in /usr/local/opt/coreutils/libexec/gnubin \
-                  /usr/local/opt/gnu-sed/libexec/gnubin \
-                  /usr/local/opt/grep/libexec/gnubin; do
-      if [ -d "$prefix" ]; then
-        PATH="${prefix}:${PATH}"
-      fi
-    done
+    path=(
+      /usr/local/opt/coreutils/libexec/gnubin(N-/)
+      /usr/local/opt/gnu-sed/libexec/gnubin(N-/)
+      /usr/local/opt/grep/libexec/gnubin(N-/)
+      $path
+    )
     ;;
 esac
 
-runtime_versions() { find -L $1 -maxdepth 2 -name bin -type d -print0| sort -Vrz| tr '\0' ':' }
+runtime_versions() {
+  find -L $1 -maxdepth 2 -name bin -type d -print0\
+    | sort -Vrz\
+    | tr '\0' ':'
+}
 
-# Emit only the latest version
-for prefix in /opt/erlang \
-              /opt/go \
-              /opt/gradle \
-              /opt/node \
-              /opt/protobuf \
-              /opt/scala /opt/sbt \
-              /opt/tmux /opt/vim; do
-  if [ -d "$prefix" ]; then
-    PATH="$(runtime_versions $prefix| cut -d ':' -f 1):${PATH}"
-  fi
-done
+() {
+  local prefix
 
-# Emit all installed versions
-for prefix in /opt/python; do
-  if [ -d "$prefix" ]; then
-    PATH="$(runtime_versions $prefix)${PATH}"
-  fi
-done
+  # Emit only the latest version
+  for prefix in /opt/erlang \
+                /opt/go \
+                /opt/gradle \
+                /opt/node \
+                /opt/protobuf \
+                /opt/scala /opt/sbt \
+                /opt/tmux \
+                /opt/vim; do
+    if [ -d "$prefix" ]; then
+      PATH="$(runtime_versions $prefix| cut -d ':' -f 1):${PATH}"
+    fi
+  done
 
-# Emit "$prefix/bin"
-for prefix in /opt/circleci $HOME/.cargo $HOME/.local $HOME/.rbenv; do
-  if [ -d "$prefix/bin" ]; then
-    PATH=$prefix/bin:$PATH
-  fi
-done
-
-unset prefix
-export PATH
+  # Emit all installed versions
+  for prefix in /opt/python; do
+    if [ -d "$prefix" ]; then
+      PATH="$(runtime_versions $prefix)${PATH}"
+    fi
+  done
+}
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "${HOME}/.local/google-cloud-sdk/path.zsh.inc" ]; then
   source "${HOME}/.local/google-cloud-sdk/path.zsh.inc";
 fi
+
+path=(
+  /opt/circleci(N-/)
+  $HOME/.cargo(N-/)
+  $HOME/.local(N-/)
+  $HOME/.rbenv(N-/)
+  $path
+)
 
 if (($+commands[go])); then
   export GOROOT="${${commands[go]}:h:h}"

.zshrc
@@ -1,12 +1,19 @@
-FPATH="${DOTFILES}/zsh/completion:${DOTFILES}/zsh/_functions:/usr/share/zsh/site-functions:${FPATH}"
+# deduplicate $FPATH (keep the first occurrence)
+typeset -U FPATH fpath
+fpath=(
+  ${DOTFILES}/zsh/completion
+  ${DOTFILES}/zsh/_functions
+  /usr/local/share/zsh/site-functions(N-/)
+  $fpath
+)
 
-for config in $DOTFILES/zsh/**/*.zsh; do
+for config in $DOTFILES/zsh/*.zsh; do
   source $config
 done
 unset config
 
 # Load custom shell functions
-autoload -U $DOTFILES/zsh/_functions/*(:t)
+autoload -Uz $DOTFILES/zsh/_functions/*(:t)
 
 if [ -S "$XDG_RUNTIME_DIR/ssh-agent.socket" ]; then
   export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
@@ -15,20 +22,20 @@ fi
 gopath $HOME/proj
 
 if [ -z "$TMUX" ] && (($+commands[tmux])); then
-  sessions=$(tmux list-session 2>/dev/null)
-  if [ -n $sessions ]; then
-    echo $sessions| sed -e $'s/^/\t/g'
-  fi
-  unset sessions
+  () {
+    local sessions
+    local choice
 
-  echo -n "> "
-  read choice
-  if tmux has-session -t $choice 2>/dev/null; then
-    exec tmux attach-session -t $choice
-  elif [ -n "$choice" ]; then
-    exec tmux new-session -s $choice
-  fi
-  unset choice
+    sessions=$(tmux list-session 2>/dev/null)
+    if [ -n $sessions ]; then
+      echo $sessions| sed -e $'s/^/\t/g'
+    fi
+
+    read 'choice?> '
+    if tmux has-session -t $choice 2>/dev/null; then
+      exec tmux attach-session -t $choice
+    elif [ -n "$choice" ]; then
+      exec tmux new-session -s $choice
+    fi
+  }
 fi
-
-autoload -U +X bashcompinit && bashcompinit

zsh/10completion.zsh
@@ -1,7 +1,7 @@
 autoload -Uz +X compinit && compinit
 
 bashcompdef=/usr/share/bash-completion/completions
-if [ -d "$bashcompdef" ] &&  [ -f "$bashcompdef/git" ]; then
+if [ -r "$bashcompdef/git" ]; then
   autoload -Uz +X bashcompinit && bashcompinit
   . "$bashcompdef/git" >/dev/null 2>&1
 fi

commit 1283d29e24b4aee26e22aa2736037091518b18c5

zsh: fix $PATH broke by 0a135a04c6a50149bfc4c9622a8c7147d9d9351b

zsh/01path.zsh
@@ -56,10 +56,11 @@ if [ -f "${HOME}/.local/google-cloud-sdk/path.zsh.inc" ]; then
 fi
 
 path=(
-  /opt/circleci(N-/)
-  $HOME/.cargo(N-/)
-  $HOME/.local(N-/)
-  $HOME/.rbenv(N-/)
+  $HOME/.local/bin(N-/)
+
+  /opt/circleci/bin(N-/)
+  $HOME/.cargo/bin(N-/)
+  $HOME/.rbenv/bin(N-/)
   $path
 )
 