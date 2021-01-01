584573cb1ad5961688f3effa2833fa07973dda80
Kohei YOSHIDA <kohei@yosida95.com>
2021-01-01 21:38:50 +0900

    zsh: move lazy completion initialization to autoloaded functions

--- zsh/10completion.zsh
+++ zsh/10completion.zsh
@@ -6,40 +6,15 @@ if [ -r "$bashcompdef/git" ]; then
   . "$bashcompdef/git" >/dev/null 2>&1
 fi
 
-if (($+commands[kubectl])); then
-  kubectl() {
-    unset -f "$0"
-    . <(kubectl completion zsh)
-    $0 "$@"
-  }
-fi
-
 if (($+commands[gcloud])); then
-  for gcloudcompdef in "${${commands[gcloud]}:h:h}/completion.zsh.inc" \
-                       "/usr/share/google-cloud-sdk/completion.zsh.inc"; do
-    if [[ -f "$gcloudcompdef" && -r "$gcloudcompdef" ]]; then
-      gcloud() {
-        unset -f "$0"
+  () {
+    local gcloudcompdef
+    for gcloudcompdef in "${${commands[gcloud]}:h:h}/completion.zsh.inc" \
+                        "/usr/share/google-cloud-sdk/completion.zsh.inc"; do
+      if [ -r "$gcloudcompdef" ]; then
         . "$gcloudcompdef"
-        $0 "$@"
-      }
-      break
-    fi
-  done
-fi
-
-if (($+commands[helm])); then
-  helm() {
-    unset -f "$0"
-    . <(helm completion zsh)
-    $0 "$@"
-  }
-fi
-
-if (($+commands[helm3])); then
-  helm3() {
-    unset -f "$0"
-    . <(helm3 completion zsh)
-    $0 "$@"
+        break
+      fi
+    done
   }
 fi
