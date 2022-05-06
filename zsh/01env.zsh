6ac8314b02e58ff5378a14409310066a9d856082
Kohei YOSHIDA <kohei@yosida95.com>
2022-05-06 18:23:25 +0900

    zsh: replace peco by fzf

--- zsh/01env.zsh
+++ zsh/01env.zsh
@@ -13,6 +13,4 @@ export LESSOPEN="| ${DOTFILES}/lesspipe.sh %s"
 export EDITOR='vim'
 export GPG_TTY=$(tty)
 
-export FZF_DEFAULT_OPTS='--layout=reverse --prompt "QUERY> " --color=dark'
-
 export JQ_COLORS="1;33:1;31:1;31:1;31:1;32:1;37:1;37"
--- zsh/50fzf.zsh
+++ zsh/50fzf.zsh
@@ -1,14 +1,16 @@
 if (($+commands[fzf])); then
-  function peco_select_history() {
+  export FZF_DEFAULT_OPTS='--layout=reverse --prompt "QUERY> " --color=dark'
+
+  function fzf_select_history() {
     BUFFER=$(fc -lnr 1| fzf --info=hidden --exact --no-sort --query "$LBUFFER"| sed 's/\\n/\n/g')
     CURSOR=$#BUFFER
     zle -R -c
   }
-  zle -N peco_select_history
-  bindkey '^R' peco_select_history
+  zle -N fzf_select_history
+  bindkey '^R' fzf_select_history
 
   if (($+commands[ghq])); then
-    function peco-src () {
+    function fzf-ghq () {
       local selected_dir=$(ghq list| fzf --info=hidden --exact --query "$LBUFFER")
       if [ -n "$selected_dir" ]; then
         BUFFER="cd $(ghq root)/${selected_dir}"
@@ -16,7 +18,7 @@ if (($+commands[fzf])); then
       fi
       zle clear-screen
     }
-    zle -N peco-src
-    bindkey '^]' peco-src
+    zle -N fzf-ghq
+    bindkey '^]' fzf-ghq
   fi
 fi
