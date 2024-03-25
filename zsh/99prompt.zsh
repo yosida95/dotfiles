7d552f05c04cd4b6ff049365ea0c85e2194d0ad5
Kohei YOSHIDA <kohei@yosida95.com>
2024-03-25 16:11:47 +0900

    zsh: use Yunocchi color rather than cwd to indicate $KEYMAP status

--- zsh/99prompt.zsh
+++ zsh/99prompt.zsh
@@ -50,8 +50,8 @@ function go_prompt_info() {
   fi
 }
 
-# Use cyan if $KEYMAP (defaults to "main" if unset) does not match "vicmd"; otherwise use blue.
-PROMPT='$(vcs_prompt_info)%F{${${${${KEYMAP-main}:#vicmd}:+cyan}:-blue}}%c %B%(?:%F{green}:%F{red})✘╹◡╹✘%f%b '
+# Set to blue if $KEYMAP (defaults to "main" if unset) matches "vicmd", to green if the last exit code is 0, otherwise to red.
+PROMPT='$(vcs_prompt_info)%F{cyan}%c %F{${${${${KEYMAP-main}:#vicmd}:+%(?:green:red)}:-blue}}✘╹◡╹✘%f '
 RPROMPT='$(python_prompt_info)$(go_prompt_info)'
 PROMPT2='%B%F{cyan}%_...%f%b '
 SPROMPT='%B%F{magenta}／人◕ ‿‿ ◕人＼ %F{red}%R%f -> %F{green}%r%f%b [n/y/a/e]? '
