ab590d11fd1f34573bb9e36551b7cadcb80e5618
Kohei YOSHIDA <kohei@yosida95.com>
2024-03-26 00:12:51 +0900

    zsh: use `fzf --zsh` if available

--- zsh/50fzf.zsh
+++ zsh/50fzf.zsh
@@ -1,22 +1,27 @@
 if (($+commands[fzf])); then
-  export FZF_DEFAULT_OPTS='--layout=reverse --prompt "QUERY> " --color=dark'
+  export FZF_DEFAULT_OPTS='--exact --layout=reverse --prompt "QUERY> " --color=dark'
+  export FZF_CTRL_R_OPTS='--info=hidden --height=100% --no-sort'
 
-  function fzf_select_history() {
-    BUFFER=$(fc -lnr 1| fzf --info=hidden --exact --no-sort --query "$LBUFFER"| sed 's/\\n/\n/g')
-    CURSOR=$#BUFFER
-    zle -R -c
-  }
-  zle -N fzf_select_history
-  bindkey '^R' fzf_select_history
+  if fzf --zsh >/dev/null 2>&1; then
+    . <(fzf --zsh)
+  else
+    function fzf_select_history() {
+      BUFFER=$(fc -lnr 1| FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} ${FZF_CTRL_R_OPTS}" fzf --query "$LBUFFER"| sed 's/\\n/\n/g')
+      CURSOR=$#BUFFER
+      zle -R -c
+    }
+    zle -N fzf_select_history
+    bindkey '^R' fzf_select_history
+  fi
 
   if (($+commands[ghq])); then
     function fzf-ghq () {
-      local selected_dir=$(ghq list| fzf --info=hidden --exact --query "$LBUFFER")
-      if [ -n "$selected_dir" ]; then
-        BUFFER="cd $(ghq root)/${selected_dir}"
+      local repo=$(ghq list| fzf --info=hidden --query "$LBUFFER")
+      if [ -n "$repo" ]; then
+        BUFFER="cd -- $(ghq root)/${(q)repo}"
         zle accept-line
       fi
-      zle clear-screen
+      zle reset-prompt
     }
     zle -N fzf-ghq
     bindkey '^]' fzf-ghq
