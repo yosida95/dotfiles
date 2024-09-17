344310a000a567e2bf1a1d739ffba401a9c1e899
Kohei YOSHIDA <kohei@yosida95.com>
2024-09-17 16:35:46 +0900

    zsh: merge 01bindkey into 00config

--- zsh/01bindkey.zsh
+++
@@ -1,8 +0,0 @@
-bindkey -d
-bindkey -v  # use vi like keybind
-
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^P" history-beginning-search-backward-end  # select previous history with Ctrl-P
-bindkey "^N" history-beginning-search-forward-end  # select next history with Ctrl-N
