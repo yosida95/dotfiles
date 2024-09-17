58bbd3ff8dd31dc2e945516aff96cfbe0d6255ac
Kohei YOSHIDA <kohei@yosida95.com>
2024-09-17 16:37:28 +0900

    zsh: merge 01homebrew into 10path

--- zsh/10path.zsh
+++ zsh/10path.zsh
@@ -1,3 +1,29 @@
+if [ "$(uname)" = "Darwin" ]; then
+  for HOMEBREW_PREFIX in /opt/homebrew /usr/local; do
+    if [ -x "$HOMEBREW_PREFIX/bin/brew" ]; then
+      . <($HOMEBREW_PREFIX/bin/brew shellenv)
+      break
+    fi
+  done
+
+  if [ -n "$HOMEBREW_PREFIX" ]; then
+    path=(
+      $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin(N-/:a)
+      $HOMEBREW_PREFIX/opt/gnu-getopt/bin(N-/:a)
+      $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin(N-/:a)
+      $HOMEBREW_PREFIX/opt/grep/libexec/gnubin(N-/:a)
+      $HOMEBREW_PREFIX/opt/openssl/bin(N-/:a)
+      $HOMEBREW_PREFIX/opt/openssl@1.1/bin(N-/:a)
+      $path
+    )
+
+    fpath=(
+      $HOMEBREW_PREFIX/share/zsh/site-functions(N-/:a)
+      $fpath
+    )
+  fi
+fi
+
 path=(
   $DOTFILES/bin
   $HOME/proj/bin(N-/:a)
