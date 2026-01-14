94032d90e19a4820cce96bb413f0a70307bd1000
Kohei YOSHIDA <kohei@yosida95.com>
2026-01-14 17:48:00 +0900

    zsh: enable terraform completion

--- zsh/20completion.zsh
+++ zsh/20completion.zsh
@@ -5,3 +5,8 @@ autoload -Uz +X compinit && compinit
 if [ -r $DOTFILES/zsh/completion/_rebar3 ]; then
   . $DOTFILES/zsh/completion/_rebar3
 fi
+
+if (($+commands[terraform])); then
+  autoload -U +X bashcompinit && bashcompinit
+  complete -o nospace -C "$commands[terraform]" terraform
+fi
